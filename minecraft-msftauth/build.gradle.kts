932f2697a0681a76097c380b1a1075479671b1be
Z User <z@container>
2026-07-23 14:37:17 +0000

    fix(build): use compilerOptions DSL instead of deprecated kotlinOptions
    Kotlin 2.3.0 removed the kotlinOptions { jvmTarget } DSL. Migrate to
    the compilerOptions DSL using JvmTarget.JVM_21 across app, minecraft,
    and minecraft-msftauth modules.

--- minecraft-msftauth/build.gradle.kts
+++ minecraft-msftauth/build.gradle.kts
@@ -25,8 +25,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_21
         targetCompatibility = JavaVersion.VERSION_21
     }
-    kotlinOptions {
-        jvmTarget = "21"
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
+        }
     }
 
 }
